35b1f0833dc6941ead51b687db5ca12093de5bd4
Mattias Lindholm <mattias@xertoz.se>
2012-09-25 13:43:48 +0200

    Add a new OOP helper class

---
+++ api/class.Helper.php
@@ -0,0 +1,33 @@
+<?php
+	/**
+	* class.Helper.php
+	* Copyright 2012 Mattias Lindholm
+	*
+	* This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+	* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter
+	* to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+	*/
+
+	namespace forge;
+
+	/**
+	* This class contains various helper functions for OOP development
+	*/
+	class Helper {
+		/**
+		 * Call an anonymous function while suppressing all exceptions
+		 * @param \Closure $function The function to be called
+		 * @return mixed Returns the function return value
+		 */
+		static public function run(\Closure $function) {
+			$arguments = func_get_args();
+			array_shift($arguments);
+			
+			try {
+				return call_user_func_array($function, $arguments);
+			}
+			catch (\Exception $e) {
+				/* Catch all exceptions, but do nothing! */
+			}
+		}
+	}
