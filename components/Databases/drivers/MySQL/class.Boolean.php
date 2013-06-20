13d094f9e8dbb8879a8a464fa2be268ddb3b3948
Mattias Lindholm <mattias@xertoz.se>
2013-06-20 11:36:25 +0200

    Add a new boolean type to MySQL

---
+++ components/Databases/drivers/MySQL/class.Boolean.php
@@ -0,0 +1,55 @@
+<?php
+	/**
+	* mysql.Boolean.php
+	* Copyright 2011-2013 Mattias Lindholm
+	*
+	* This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+	* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter
+	* to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+	*/
+
+	namespace forge\components\Databases\drivers\MySQL;
+
+	/**
+	* MySQL data type Int
+	*/
+	class Boolean extends \forge\components\Databases\Type {
+		/**
+		* Default value
+		*/
+		protected $default = '\'0\'';
+
+		/**
+		 * Default column length
+		 */
+		protected $length = 1;
+
+		/**
+		* Prevent NULL values
+		*/
+		protected $null = false;
+
+		/**
+		* SQL type
+		* @var string
+		*/
+		protected $type = 'tinyint';
+
+		/**
+		* Get the PDO data type of this column
+		* @return int
+		*/
+		public function getDataType() {
+			return \PDO::PARAM_BOOL;
+		}
+
+		/**
+		* Set a new value
+		* @param mixed New value
+		* @return void
+		* @throws Exception
+		*/
+		public function set($value) {
+			$this->value = (int)(bool)$value;
+		}
+	}
