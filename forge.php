f8737c0b3ac5395e43963755c32f705be7a904d8
Mattias Lindholm <mattias@xertoz.se>
2014-08-27 20:10:33 +0200

    If gettext isn't installed we can fall back on a dummy function which just returns its input.

--- forge.php
+++ forge.php
@@ -8,75 +8,84 @@
 	* to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 	*/
 
-	namespace forge;
-	
-	// Set the version number of this software
-	define('FORGE_VERSION', '5.0.0-dev');
-	
-	// Set the root path for the Forge system
-	define('FORGE_PATH', __DIR__);
-	
-	// As a time zone must be set, use UTC for convenience
-	date_default_timezone_set('UTC');
+	namespace forge {
+		// Set the version number of this software
+		define('FORGE_VERSION', '5.0.0-dev');
 
-	/**
-	* Throw errors generated
-	* This function will ALWAYS throw an Exception
-	* @param int $errno
-	* @param string $msg
-	* @param string $file
-	* @param int $line
-	* @return void
-	* @throws Exception
-	*/
-	function error_handler($errno, $msg, $file, $line) {
-		throw new \Exception($file.'('.$line.'): '.$msg, $errno);
-	}
-	
-	// Bind the error handler
-	set_error_handler('forge\error_handler', E_ALL);
-	
-	/**
-	 * An autoloader for the Forge environment
-	 * @param string $subject The name of the class that was not loaded yet
-	 * @internal
-	 */
-	function autoload($subject) {
-		// Split the subject up for inspection
-		$namespace = explode('\\', $subject);
-		
-		// The root namespace must be forge, and we can discard it
-		if (array_shift($namespace) != 'forge')
-			return;
-		
-		// Fetch the class name out of the array
-		$class = array_pop($namespace);
-		
-		// Depending on the first subspace, choose a path
-		switch (array_shift($namespace)) {
-			default:
-				$path = 'api';
-				$type = 'class';
-				break;
-			
-			case 'components':
-				$path = 'components/'.(($count = count($namespace)) ? implode('/', $namespace) : $class);
-				$type = $count ? 'class' : 'com';
-				break;
-			
-			case 'modules':
-				$path = 'modules/'.(($count = count($namespace)) ? implode('/', $namespace) : $class);
-				$type = $count ? 'class' : 'mod';
-				break;
+		// Set the root path for the Forge system
+		define('FORGE_PATH', __DIR__);
+
+		// As a time zone must be set, use UTC for convenience
+		date_default_timezone_set('UTC');
+
+		/**
+		* Throw errors generated
+		* This function will ALWAYS throw an Exception
+		* @param int $errno
+		* @param string $msg
+		* @param string $file
+		* @param int $line
+		* @return void
+		* @throws Exception
+		*/
+		function error_handler($errno, $msg, $file, $line) {
+			throw new \Exception($file.'('.$line.'): '.$msg, $errno);
 		}
-		
-		// Build a file path & name
-		$file = FORGE_PATH.'/'.$path.'/'.$type.'.'.$class.'.php';
-		
-		// If it exists, include it
-		if (file_exists($file))
-			require_once $file;
+
+		// Bind the error handler
+		set_error_handler('forge\error_handler', E_ALL);
+
+		/**
+		 * An autoloader for the Forge environment
+		 * @param string $subject The name of the class that was not loaded yet
+		 * @internal
+		 */
+		function autoload($subject) {
+			// Split the subject up for inspection
+			$namespace = explode('\\', $subject);
+
+			// The root namespace must be forge, and we can discard it
+			if (array_shift($namespace) != 'forge')
+				return;
+
+			// Fetch the class name out of the array
+			$class = array_pop($namespace);
+
+			// Depending on the first subspace, choose a path
+			switch (array_shift($namespace)) {
+				default:
+					$path = 'api';
+					$type = 'class';
+					break;
+
+				case 'components':
+					$path = 'components/'.(($count = count($namespace)) ? implode('/', $namespace) : $class);
+					$type = $count ? 'class' : 'com';
+					break;
+
+				case 'modules':
+					$path = 'modules/'.(($count = count($namespace)) ? implode('/', $namespace) : $class);
+					$type = $count ? 'class' : 'mod';
+					break;
+			}
+
+			// Build a file path & name
+			$file = FORGE_PATH.'/'.$path.'/'.$type.'.'.$class.'.php';
+
+			// If it exists, include it
+			if (file_exists($file))
+				require_once $file;
+		}
+
+		// Set up the autoloader for Forge
+		spl_autoload_register('forge\autoload', true, false);
 	}
-	
-	// Set up the autoloader for Forge
-	spl_autoload_register('forge\autoload', true, false);
+
+	namespace {
+		// Make sure we can produce any output even though gettext isn't supported
+		if (!function_exists('_')) {
+			function _() {
+				return func_get_args()[0];
+			}
+		}
+	}
