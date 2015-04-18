1f102257aea4005a1b5d2a958e1e594e599c03d4
Mattias Lindholm <mattias@xertoz.se>
2015-04-18 11:51:26 +0200

    Add a helper class for validating user input over HTTP

---
+++ api/class.Post.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	* class.Post.php
+	* Copyright 2015 Mattias Lindholm
+	*
+	* This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+	* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter
+	* to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+	*/
+
+	namespace forge;
+	
+	/**
+	 * Helper class for handling POST data from HTTP requests
+	 */
+	class Post extends Input {
+		static protected function getType() {
+			return \INPUT_POST;
+		}
+	}
