ba5716cd4857747df3a658f59e87be39838147a9
Mattias Lindholm <mattias@xertoz.se>
2019-09-24 12:39:05 +0200

    Create a prettier debug page

--- errors/dump.php
+++ errors/dump.php
@@ -1,8 +1,13 @@
 <?php
+	// TODO: Fix /admin/ links in this file
+	header('Cache-Control: no-store');
+
 	$stack = function($array,$title) {
 ?>
-		<h1><?php echo htmlentities($title); ?></h1>
-		<table>
+<div class="box box-info">
+	<div class="box-header with-border"><h1 class="box-title"><?php echo htmlentities($title); ?></h1></div>
+	<div class="box-body">
+		<table class="table table-striped">
 			<thead>
 				<tr>
 					<th>Parameter</th>
@@ -17,15 +22,17 @@
 					</tr>
 				<?php endforeach; else: ?>
 					<tr>
-						<td><i>Empty</i></td>
+						<td colspan="100%"><i>Empty</i></td>
 					</tr>
 				<?php endif; ?>
 			</tbody>
 		</table>
+	</div>
+</div>
 <?php
 	}
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Forge <?php echo FORGE_VERSION; ?> - Debug Stack <?php echo date('Y-m-d H:i:s'); ?></title>
@@ -39,39 +46,76 @@
 				padding-right:1em;
 			}
 		</style>
+		<link href="/vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/vendor/almasaeed2010/adminlte/dist/css/AdminLTE.min.css" rel="stylesheet">
 	</head>
-	<body>
-		<h1>Exception</h1>
-		<p><b><?php echo get_class($e); ?></b> was thrown in <b><?php echo htmlentities($e->getFile()); ?></b> at line <b><?php echo htmlentities($e->getLine()); ?></b> with the message <b><?php echo htmlentities($e->getMessage()); ?></b> and error code <b><?php echo htmlentities($e->getCode()); ?></b></p>
-		<h2>Stack trace</h2>
-		<table>
-			<thead>
-				<tr>
-					<th>Function</th>
-					<th>Arguments</th>
-					<th>File</th>
-					<th>Line</th>
-				</tr>
-			</thead>
-			<?php foreach ($e->getTrace() as $trace): ?>
-				<tr>
-					<td><?php echo isset($trace['function']) ? htmlentities($trace['function']) : null; ?></td>
-					<td>
-						<?php if (isset($trace['args'])): ?>
-							<table>
-								<?php foreach ($trace['args'] as $arg): ?>
-									<tr>
-										<td><?php echo htmlentities(var_export($arg)); ?></td>
-									</tr>
-								<?php endforeach; ?>
-							</table>
-							<?php endif; ?>
-					</td>
-					<td><?php echo isset($trace['file']) ? htmlentities($trace['file']) : null; ?></td>
-					<td><?php echo isset($trace['line']) ? htmlentities($trace['line']) : null; ?></td>
-				</tr>
-			<?php endforeach; ?>
-		</table>
+	<body class="login-page" style="padding:50px;">
+		<div class="login-logo"><b>Forge</b> <?=FORGE_VERSION?></div>
+		<div class="box box-danger">
+			<div class="box-header with-border"><h1 class="box-title">Exception</h1></div>
+			<div class="box-body">
+				<p><b><?php echo get_class($e); ?></b> was thrown in <b><?php echo htmlentities($e->getFile()); ?></b> at line <b><?php echo htmlentities($e->getLine()); ?></b> with the message <b><?php echo htmlentities($e->getMessage()); ?></b> and error code <b><?php echo htmlentities($e->getCode()); ?></b></p>
+			</div>
+		</div>
+		<div class="box box-warning">
+			<div class="box-header with-border"><h1 class="box-title">Stack trace</h1></div>
+			<div class="box-body">
+				<table class="table table-condensed">
+					<thead>
+					<tr>
+						<th>Function</th>
+						<th>Arguments</th>
+						<th>File</th>
+						<th>Line</th>
+					</tr>
+					</thead>
+					<tbody>
+					<?php foreach ($e->getTrace() as $trace): ?>
+						<tr>
+							<td>
+								<?php if (!empty($trace['class'])): ?>
+								<a href="/admin/Documentation/lookup?class=<?=urlencode($trace['class'])?>&method=<?=$trace['function']?>"><?=htmlentities($trace['class'])?>::<?=htmlentities($trace['function'])?></a>
+								<?php else: ?>
+								<a href="/admin/Documentation/lookup?&function=<?=$trace['function']?>"><?=htmlentities($trace['function'])?></a></td>
+								<?php endif; ?>
+							<td>
+								<?php if (isset($trace['args'])): ?>
+									<table class="table table-condensed">
+										<?php foreach ($trace['args'] as $arg): ?>
+											<tr>
+												<td><?php echo gettype($arg); ?></td>
+												<td><?php
+													switch (gettype($arg)) {
+														default:
+															echo '<span title="'.htmlentities($arg).'">'.htmlentities(substr($arg, 0, 32)).'</span>';
+															break;
+														case 'object':
+															echo '<a href="/admin/Documentation/lookup?class='.urlencode(get_class($arg)).'">'.get_class($arg).'</a>';
+															break;
+														case 'array':
+														case 'null':
+															break;
+													}
+												?></td>
+											</tr>
+										<?php endforeach; ?>
+									</table>
+								<?php endif; ?>
+							</td>
+							<td><?=htmlentities($trace['file'])?></td>
+							<td><?php echo isset($trace['line']) ? htmlentities($trace['line']) : null; ?></td>
+						</tr>
+					<?php endforeach; ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="box box-primary">
+			<div class="box-header with-border"><h1 class="box-title">Source</h1></div>
+			<div class="box-body">
+				<pre><?=str_replace(["\n", '<br />'], '', highlight_string(file_get_contents($e->getFile()), true))?></pre>
+			</div>
+		</div>
 		<?php $stack($_SERVER,'$_SERVER'); ?>
 		<?php $stack($_SESSION,'$_SESSION'); ?>
 		<?php $stack($_COOKIE,'$_COOKIE'); ?>
